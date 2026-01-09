39217b355bdac1825165486eacba10c0d8553129
pyrozhenkko <pyrozhenkko@gmail.com>
2026-01-09 22:59:46 +0200

    testing 2 repos

--- src/main/java/com/epam/rd/autocode/spring/project/BookStoreServiceSolutionApplication.java
+++ src/main/java/com/epam/rd/autocode/spring/project/BookStoreServiceSolutionApplication.java
@@ -9,5 +9,6 @@ public class BookStoreServiceSolutionApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookStoreServiceSolutionApplication.class, args);
     }
+    // tested one
 
 }
